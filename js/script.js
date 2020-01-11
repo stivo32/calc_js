c994e45a5d6bacf83702ddf85dad48d706cab9db
stivo32 <stivo32@gmail.com>
2020-01-11 22:18:21 +0300

    big commit with modified front and first commit of logic

--- js/script.js
+++ js/script.js
@@ -1,68 +1,185 @@
 const app = document.querySelector('#app');
 
-
-const addDisplayString = () => {
+const addDisplayString = (className='') => {
     const element = document.createElement('input');
     element.setAttribute('type', 'text');
     element.setAttribute('id', 'display');
+    if (className) {
+        element.classList.add(className);
+    }
     return element;
 };
 
-const addButton = (sign, descr) => {
+const addButton =(sign, descr, className='') => {
     const element = document.createElement('button');
     element.setAttribute('id', descr);
     element.textContent = sign;
+    if (className) {
+        element.classList.add(className);
+    }
     return element;
 };
 
-const wrapElement = element => {
-    const container = document.createElement('div');
-    container.setAttribute('class', 'element-wrapper');
-    container.appendChild(element);
-    return container;
-};
+class CalculatorFront {
+    constructor(app) {
+        this.app = app;
+        this.logic = null;
+        this.elements = {};
+        this.classes = {
+            number: 'number',
+            output: 'output',
+            operation: 'operation',
+            point: 'point',
+        };
+        this.firstNumber = '';
+        this.secondNumber = '';
+        this.operation = '';
+    }
 
-const displayField = addDisplayString();
-const one = addButton('1', 'one');
-const two = addButton('2', 'two');
-const three = addButton('3', 'three');
-const four = addButton('4', 'four');
-const five = addButton('5', 'five');
-const six = addButton('6', 'six');
-const seven = addButton('7', 'seven');
-const eight = addButton('8', 'eight');
-const nine = addButton('9', 'nine');
-const zero = addButton('0', 'zero');
-const plus = addButton('+', 'plus');
-const minus = addButton('-', 'minus');
-const multiply = addButton('*', 'multiply');
-const divide = addButton('/', 'divide');
-const equal = addButton('=', 'equal');
-const point = addButton('.', 'point');
-const clear = addButton('clear', 'clear');
+    setFrontend() {
+        this.createElements();
+        this.displayElements();
+        this.setListeners();
+    }
 
-const elements = {
-    displayField,
-    divide,
-    multiply,
-    minus,
-    plus,
-    seven,
-    eight,
-    nine,
-    clear,
-    four,
-    five,
-    six,
-    point,
-    one,
-    two,
-    three,
-    equal,
-    zero
-};
+    setLogic(logic) {
+        this.logic = logic;
+    }
 
-Object.values(elements).forEach(element => app.appendChild(element));
+    createElements() {
+        this.elements['displayField'] = addDisplayString(this.classes['output']);
+        this.elements['one'] = addButton('1', 'one', this.classes['number']);
+        this.elements['two'] = addButton('2', 'two', this.classes['number']);
+        this.elements['three'] = addButton('3', 'three', this.classes['number']);
+        this.elements['four'] = addButton('4', 'four', this.classes['number']);
+        this.elements['five'] = addButton('5', 'five', this.classes['number']);
+        this.elements['six'] = addButton('6', 'six', this.classes['number']);
+        this.elements['seven'] = addButton('7', 'seven', this.classes['number']);
+        this.elements['eight'] = addButton('8', 'eight', this.classes['number']);
+        this.elements['clear'] = addButton('clear', 'clear', this.classes['operation']);
+        this.elements['nine'] = addButton('9', 'nine', this.classes['number']);
+        this.elements['zero'] = addButton('0', 'zero', this.classes['number']);
+        this.elements['plus'] = addButton('+', 'plus', this.classes['operation']);
+        this.elements['minus'] = addButton('-', 'minus', this.classes['operation']);
+        this.elements['multiply'] = addButton('*', 'multiply', this.classes['operation']);
+        this.elements['divide'] = addButton('/', 'divide', this.classes['operation']);
+        this.elements['equal'] = addButton('=', 'equal', this.classes['operation']);
+        this.elements['point'] = addButton('.', 'point', this.classes['point']);
+    }
+
+    displayElements() {
+        Object.values(this.elements).forEach((element) => this.app.appendChild(element), this);
+    }
+
+    setListeners() {
+        const displayField = this.elements['displayField'];
+        document.onkeydown = event => {
+            const key = event.key;
+            const numbers = '1234567890';
+            if (numbers.includes(key)) displayField.value += key;
+            if (key === '.' && this.checkIfPointAllowed()) displayField.value += key;
+            if (displayField.value.length === 0 && key === '-') displayField.value += key;
+            if (key === 'Backspace') displayField.value = displayField.value.slice(0, -1);
+            if (['/', '*', '-', '+', 'Enter'].includes(key)) this.perform(key);
+        };
+        for (let key of Object.keys(this.elements)){
+            let element = this.elements[key];
+            element.onclick = () => {
+                if (element.classList.contains(this.classes['number'])) {
+                    displayField.value += element.textContent;
+                } else if (element.classList.contains(this.classes['point'])){
+                    displayField.value += this.checkIfPointAllowed()? '.': '';
+                } else if (element.classList.contains(this.classes['operation'])) {
+                    this.perform(element.textContent);
+                }
+            }
+        }
+    }
+
+    perform(key) {
+        if (key === 'clear') {
+            this.elements['displayField'].value = '';
+            this.firstNumber = '';
+            this.secondNumber = '';
+            this.operation = '';
+            return
+        }
+        if (['/', '*', '+', '-'].includes(key)){
+            if (this.operation) return;
+            this.operation = key;
+            this.firstNumber = this.elements['displayField'].value || '0';
+        } else if (key === '') {
+
+        }
+    }
+
+    checkIfPointAllowed() {
+        return this.elements['displayField'].value.length > 0 && !this.elements['displayField'].value.includes('.')
+    }
+
+}
+
+class Calculator {
+    constructor(frontend) {
+        this.frontend =
+        this.firstNumber = null;
+        this.secondNumber = null;
+        this.operation = null;
+        this.result = NaN;
+        this.mapper = {
+            '+': this.add,
+            '-': this.subtract,
+            '/': this.divide,
+            '*': this.multiple
+        }
+    }
+
+    compute() {
+        return this.mapper[this.operation]()
+    }
+
+    add() {
+        return this.firstNumber + this.secondNumber
+    }
+
+    subtract() {
+        return this.firstNumber - this.secondNumber
+    }
+
+    multiple() {
+        return this.firstNumber * this.secondNumber
+    }
+
+    divide() {
+        return this.firstNumber / this.secondNumber
+    }
+
+    clear() {
+        this.firstNumber = null;
+        this.secondNumber = null;
+        this.operation = null;
+    }
+
+
+}
+
+calc = new Calculator();
+front = new CalculatorFront(app);
+front.setFrontend();
+front.setLogic(calc);
+
+
+// equal.onclick = () => {
+//     if (displayField.value === '') {
+//         return
+//     }
+//     calc.secondNumber = displayField.value;
+//     if ([calc.firstNumber, calc.secondNumber, calc.operation].every(value => value !== null)) {
+//         displayField.value = calc.compute();
+//         calc.clear();
+//     }
+//
+// }
 
 
 
