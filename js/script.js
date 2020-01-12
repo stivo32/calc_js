9d9413bd23272772d46fff1017182a56b66550a2
stivo32 <stivo32@gmail.com>
2020-01-12 18:00:18 +0300

    Много изменений. Добавил якори для ряда кнопок. Добавил операцию изменения знака. Фиксация изменений на конец дня.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 const app = document.querySelector('#app');
 
-const addDisplayString = (className='') => {
-    const element = document.createElement('input');
-    element.setAttribute('type', 'text');
-    element.setAttribute('id', 'display');
-    if (className) {
-        element.classList.add(className);
-    }
-    return element;
-};
+// const addDisplayString = (className = '') => {
+//     const element = document.createElement('div');
+//     element.setAttribute('id', 'display');
+//     if (className) {
+//         element.classList.add(className);
+//     }
+//     return element;
+// };
 
-const addButton =(sign, descr, className='') => {
-    const element = document.createElement('button');
-    element.setAttribute('id', descr);
-    element.textContent = sign;
-    if (className) {
-        element.classList.add(className);
-    }
+const addElement = (tag, content='', id='', className='') => {
+    const element = document.createElement(tag);
+    if (id) element.setAttribute('id', id);
+    if (className) element.classList.add(className);
+    element.textContent = content;
     return element;
 };
 
@@ -30,6 +27,11 @@ class CalculatorFront {
             output: 'output',
             operation: 'operation',
             point: 'point',
+            backspace: 'backspace',
+        };
+        this.operationMapper = {
+            'Enter': '=',
+            'Backspace': '<='
         };
         this.firstNumber = '';
         this.secondNumber = '';
@@ -47,24 +49,26 @@ class CalculatorFront {
     }
 
     createElements() {
-        this.elements['displayField'] = addDisplayString(this.classes['output']);
-        this.elements['one'] = addButton('1', 'one', this.classes['number']);
-        this.elements['two'] = addButton('2', 'two', this.classes['number']);
-        this.elements['three'] = addButton('3', 'three', this.classes['number']);
-        this.elements['four'] = addButton('4', 'four', this.classes['number']);
-        this.elements['five'] = addButton('5', 'five', this.classes['number']);
-        this.elements['six'] = addButton('6', 'six', this.classes['number']);
-        this.elements['seven'] = addButton('7', 'seven', this.classes['number']);
-        this.elements['eight'] = addButton('8', 'eight', this.classes['number']);
-        this.elements['clear'] = addButton('clear', 'clear', this.classes['operation']);
-        this.elements['nine'] = addButton('9', 'nine', this.classes['number']);
-        this.elements['zero'] = addButton('0', 'zero', this.classes['number']);
-        this.elements['plus'] = addButton('+', 'plus', this.classes['operation']);
-        this.elements['minus'] = addButton('-', 'minus', this.classes['operation']);
-        this.elements['multiply'] = addButton('*', 'multiply', this.classes['operation']);
-        this.elements['divide'] = addButton('/', 'divide', this.classes['operation']);
-        this.elements['equal'] = addButton('=', 'equal', this.classes['operation']);
-        this.elements['point'] = addButton('.', 'point', this.classes['point']);
+        this.elements['displayField'] = addElement('div', '','display', this.classes['output']);
+        this.elements['divide'] = addElement('button', '/', 'divide', this.classes['operation']);
+        this.elements['multiply'] = addElement('button','*', 'multiply', this.classes['operation']);
+        this.elements['plus'] = addElement('button','+', 'plus', this.classes['operation']);
+        this.elements['minus'] = addElement('button','-', 'minus', this.classes['operation']);
+        this.elements['clear'] = addElement('button','clear', 'clear', this.classes['operation']);
+        this.elements['equal'] = addElement('button','=', 'equal', this.classes['operation']);
+        this.elements['one'] = addElement('button','1', 'one', this.classes['number']);
+        this.elements['two'] = addElement('button','2', 'two', this.classes['number']);
+        this.elements['three'] = addElement('button','3', 'three', this.classes['number']);
+        this.elements['four'] = addElement('button','4', 'four', this.classes['number']);
+        this.elements['five'] = addElement('button','5', 'five', this.classes['number']);
+        this.elements['six'] = addElement('button','6', 'six', this.classes['number']);
+        this.elements['seven'] = addElement('button','7', 'seven', this.classes['number']);
+        this.elements['eight'] = addElement('button','8', 'eight', this.classes['number']);
+        this.elements['nine'] = addElement('button','9', 'nine', this.classes['number']);
+        this.elements['zero'] = addElement('button','0', 'zero', this.classes['number']);
+        this.elements['sign'] = addElement('button','+/-', 'sign', this.classes['operation']);
+        this.elements['point'] = addElement('button','.', 'point', this.classes['point']);
+        this.elements['backspace'] = addElement('button','<=', 'backspace', this.classes['operation']);
     }
 
     displayElements() {
@@ -73,22 +77,23 @@ class CalculatorFront {
 
     setListeners() {
         const displayField = this.elements['displayField'];
+        // Вешаем обработчик для нажатия клавиш.
         document.onkeydown = event => {
             const key = event.key;
             const numbers = '1234567890';
-            if (numbers.includes(key)) displayField.value += key;
-            if (key === '.' && this.checkIfPointAllowed()) displayField.value += key;
-            if (displayField.value.length === 0 && key === '-') displayField.value += key;
-            if (key === 'Backspace') displayField.value = displayField.value.slice(0, -1);
-            if (['/', '*', '-', '+', 'Enter'].includes(key)) this.perform(key);
+            if (numbers.includes(key)) displayField.innerHTML += key;
+            if (key === '.' && this.checkIfPointAllowed()) displayField.innerHTML += key;
+            if (displayField.innerHTML.length === 0 && key === '-') displayField.innerHTML += key;
+            if (['/', '*', '-', '+', 'Enter', 'Backspace'].includes(key)) this.perform(key);
         };
-        for (let key of Object.keys(this.elements)){
+        // вешаем обработчик на каждую кнопку калькулятора.
+        for (let key of Object.keys(this.elements)) {
             let element = this.elements[key];
             element.onclick = () => {
                 if (element.classList.contains(this.classes['number'])) {
-                    displayField.value += element.textContent;
-                } else if (element.classList.contains(this.classes['point'])){
-                    displayField.value += this.checkIfPointAllowed()? '.': '';
+                    displayField.innerHTML += element.textContent;
+                } else if (element.classList.contains(this.classes['point'])) {
+                    displayField.innerHTML += this.checkIfPointAllowed() ? '.' : '';
                 } else if (element.classList.contains(this.classes['operation'])) {
                     this.perform(element.textContent);
                 }
@@ -96,68 +101,68 @@ class CalculatorFront {
         }
     }
 
+
     perform(key) {
-        if (key === 'clear') {
-            this.elements['displayField'].value = '';
-            this.firstNumber = '';
-            this.secondNumber = '';
-            this.operation = '';
-            return
+        if (!this.logic) throw Error('You need to setup some logic');
+
+        if (key in this.operationMapper) {
+            key = this.operationMapper[key];
         }
-        if (['/', '*', '+', '-'].includes(key)){
+
+        if (key === 'clear') {
+            this.clearOutput();
+            this.clearInputVariables();
+        } else if (key === '+/-') {
+            if (this.elements['displayField'].innerHTML[0] === '-') {
+                this.elements['displayField'].innerHTML = this.elements['displayField'].innerHTML.slice(1);
+            } else this.elements['displayField'].innerHTML = `-${this.elements['displayField'].innerHTML}`;
+        } else if (key === '<=') {
+            this.elements['displayField'].innerHTML = this.elements['displayField'].innerHTML.slice(0, -1);
+        }
+        if (['/', '*', '+', '-'].includes(key)) {
             if (this.operation) return;
             this.operation = key;
-            this.firstNumber = this.elements['displayField'].value || '0';
-        } else if (key === '') {
-
+            this.firstNumber = this.elements['displayField'].innerHTML || '0';
+            this.elements['displayField'].innerHTML = '';
+        } else if (key === '=') {
+            console.log(this.firstNumber, this.operation, this.secondNumber);
+            if (!this.elements['displayField'].innerHTML) return;
+            this.secondNumber = this.elements['displayField'].innerHTML;
+            this.elements['displayField'].innerHTML = this.logic.compute(this.firstNumber, this.secondNumber, this.operation);
+            this.clearInputVariables();
+            this.firstNumber = this.elements['displayField'].innerHTML;
         }
     }
 
+    clearOutput() {
+        this.elements['displayField'].innerHTML = '';
+    }
+
+    clearInputVariables() {
+        this.firstNumber = '';
+        this.secondNumber = '';
+        this.operation = '';
+    }
+
     checkIfPointAllowed() {
-        return this.elements['displayField'].value.length > 0 && !this.elements['displayField'].value.includes('.')
+        return this.elements['displayField'].innerHTML.length > 0 && !this.elements['displayField'].innerHTML.includes('.')
     }
 
 }
 
 class Calculator {
-    constructor(frontend) {
-        this.frontend =
-        this.firstNumber = null;
-        this.secondNumber = null;
-        this.operation = null;
-        this.result = NaN;
+    constructor() {
         this.mapper = {
-            '+': this.add,
-            '-': this.subtract,
-            '/': this.divide,
-            '*': this.multiple
-        }
+            '+': (first, second) => first + second,
+            '-': (first, second) => first - second,
+            '/': (first, second) => first / second,
+            '*': (first, second) => first * second,
+        };
+
     }
 
-    compute() {
-        return this.mapper[this.operation]()
-    }
-
-    add() {
-        return this.firstNumber + this.secondNumber
-    }
-
-    subtract() {
-        return this.firstNumber - this.secondNumber
-    }
-
-    multiple() {
-        return this.firstNumber * this.secondNumber
-    }
-
-    divide() {
-        return this.firstNumber / this.secondNumber
-    }
-
-    clear() {
-        this.firstNumber = null;
-        this.secondNumber = null;
-        this.operation = null;
+    compute(firstNumber, secondNumber, operation) {
+        return this.mapper[operation](parseFloat(firstNumber), parseFloat(secondNumber))
     }
 
 
@@ -168,18 +173,6 @@ front = new CalculatorFront(app);
 front.setFrontend();
 front.setLogic(calc);
 
-
-// equal.onclick = () => {
-//     if (displayField.value === '') {
-//         return
-//     }
-//     calc.secondNumber = displayField.value;
-//     if ([calc.firstNumber, calc.secondNumber, calc.operation].every(value => value !== null)) {
-//         displayField.value = calc.compute();
-//         calc.clear();
-//     }
-//
-// }
 
 
 
